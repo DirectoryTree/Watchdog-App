1d6a6edb31538e3eb0992fb3f69e6bf422f2bb13
Steve Bauman <steven_bauman@outlook.com>
2020-04-14 23:09:56 -0400

    New prod asset build

--- app/Http/Controllers/WatcherDogsController.php
+++ app/Http/Controllers/WatcherDogsController.php
@@ -7,6 +7,14 @@ use DirectoryTree\Watchdog\LdapWatcher;
 
 class WatcherDogsController extends Controller
 {
+    /**
+     * Displays a list of the notifications sent by the watchdog.
+     *
+     * @param LdapWatcher $watcher
+     * @param Watchdog    $watchdog
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(LdapWatcher $watcher, Watchdog $watchdog)
     {
         $notifications = $watchdog->notifications()
