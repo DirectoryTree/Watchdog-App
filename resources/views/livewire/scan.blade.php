567c8ad4e6f67adae38537c8b95f04d2b265d0db
Steve Bauman <steven_bauman@outlook.com>
2020-04-15 17:37:04 -0400

    Updated view to support scan failures

--- resources/views/livewire/scan.blade.php
+++ resources/views/livewire/scan.blade.php
@@ -1,18 +1,18 @@
-<div {{ $scan->completed_at ? '' : 'wire:poll' }} class="card border-0 shadow-sm mb-4">
+<div {{ $scan->completed_at || $scan->failed ? '' : 'wire:poll' }} class="card border-0 shadow-sm mb-4">
     <div class="card-header bg-light d-flex justify-content-between">
         <h5 class="mb-0 flex-shrink-1 mr-4 text-muted"># {{ $scan->id }}</h5>
 
         <div class="d-flex align-items-center">
             @if($scan->completed_at)
                 <span class="badge badge-success">Completed</span>
+            @elseif($scan->failed)
+                <span class="badge badge-danger">Error</span>
             @elseif($scan->started_at)
                 <span class="badge badge-warning">Running</span>
 
                 <div class="spinner-border spinner-border-sm ml-2" role="status">
                     <span class="sr-only">Loading...</span>
                 </div>
-            @elseif($scan->started_at && !$scan->completed_at)
-                <span class="badge badge-danger">Error</span>
             @else
                 <span class="badge badge-info">Queued...</span>
             @endif
@@ -26,30 +26,26 @@
             </div>
 
             <div class="text-muted">
-                <i class="fas fa-clock"></i> {{ $scan->duration ?? 'Waiting...' }}
+                <i class="fas fa-clock"></i>
+
+                @if($scan->failed)
+                    <em>N/A</em>
+                @else
+                    {{ $scan->duration ?? 'Waiting...' }}
+                @endif
 
                 @if($scan->completed_at)
                     | Completed on <x-date-time :date="$scan->completed_at"></x-date-time>
                 @endif
             </div>
         </div>
-
-        @if($scan->failed)
-            <details class="bg-secondary rounded mt-2 p-2">
-                <summary>Details</summary>
-
-                <div class="mt-2">
-                    {{ $scan->message }}
-                </div>
-            </details>
-        @endif
     </div>
     @unless($scan->completed_at)
         <div class="card-footer bg-light">
             <ul class="list-group shadow-sm rounded d-flex list-group-horizontal-xl text-center">
                 @foreach($this->states as $state => $name)
                     @if($scan->progress->where('state', $state)->isNotEmpty())
-                        <li class="list-group-item bg-success flex-grow-1 p-2">
+                        <li class="list-group-item {{ $scan->failed ? 'bg-danger' : 'bg-success' }} flex-grow-1 p-2">
                             <i class="fas fa-check-circle"></i> {{ $name }}
                         </li>
                     @else
@@ -61,4 +57,16 @@
             </ul>
         </div>
     @endunless
+
+    @if($scan->failed)
+        <div class="card-footer bg-light">
+            <details>
+                <summary>Details</summary>
+
+                <div class="bg-secondary rounded mt-2 p-2">
+                    {{ $scan->message }}
+                </div>
+            </details>
+        </div>
+    @endif
 </div>
