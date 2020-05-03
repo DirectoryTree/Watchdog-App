57158948464b76fd829d45ff490ee03e0b46b28d
Steve Bauman <steven_bauman@outlook.com>
2020-05-02 22:43:55 -0400

    Added turbolinks

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,37 @@
 require('./bootstrap');
 import * as Ladda from 'ladda';
+import Turbolinks from 'turbolinks';
 
-Ladda.bind('button[type=submit]');
+// Boot Turbolinks...
+Turbolinks.start();
 
-$(function () {
+// Persist scroll position with Turbolinks.
+Turbolinks.scroll = {};
+
+document.addEventListener('turbolinks:load', () => {
+    // Enable ladda.
+    Ladda.bind('button[type=submit]');
+
+    let elements = document.querySelectorAll("[data-turbolinks-scroll]");
+
+    elements.forEach((element) => {
+        element.addEventListener("click", () => {
+            Turbolinks.scroll['top'] = document.scrollingElement.scrollTop;
+        });
+
+        element.addEventListener("submit", () => {
+            Turbolinks.scroll['top'] = document.scrollingElement.scrollTop;
+        });
+    });
+
     $('[data-toggle="tooltip"]').tooltip();
     $('[data-toggle="popover"]').popover();
+});
+
+document.addEventListener('turbolinks:render', () => {
+    if (Turbolinks.scroll['top']) {
+        document.scrollingElement.scrollTo(0, Turbolinks.scroll['top']);
+    }
+
+    Turbolinks.scroll = {};
 });
